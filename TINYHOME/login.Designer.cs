48306fc6f3814236a1d079a949631dcdc83e2472
Issamhanzaz <43036997+Issamhanzaz@users.noreply.github.com>
2018-12-04 15:51:12 +0100

    Heey brammetje :)

--- TINYHOME/login.Designer.cs
+++ TINYHOME/login.Designer.cs
@@ -37,23 +37,26 @@
             // 
             // txtGebruikersnaam
             // 
-            this.txtGebruikersnaam.Location = new System.Drawing.Point(12, 25);
+            this.txtGebruikersnaam.Location = new System.Drawing.Point(16, 31);
+            this.txtGebruikersnaam.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtGebruikersnaam.Name = "txtGebruikersnaam";
-            this.txtGebruikersnaam.Size = new System.Drawing.Size(129, 20);
+            this.txtGebruikersnaam.Size = new System.Drawing.Size(171, 22);
             this.txtGebruikersnaam.TabIndex = 0;
             // 
             // txtWachtwoord
             // 
-            this.txtWachtwoord.Location = new System.Drawing.Point(12, 76);
+            this.txtWachtwoord.Location = new System.Drawing.Point(16, 94);
+            this.txtWachtwoord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtWachtwoord.Name = "txtWachtwoord";
-            this.txtWachtwoord.Size = new System.Drawing.Size(129, 20);
+            this.txtWachtwoord.Size = new System.Drawing.Size(171, 22);
             this.txtWachtwoord.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(12, 116);
+            this.btnLogin.Location = new System.Drawing.Point(58, 111);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(129, 45);
+            this.btnLogin.Size = new System.Drawing.Size(172, 55);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -61,31 +64,34 @@
             // lblGebruikersnaam
             // 
             this.lblGebruikersnaam.AutoSize = true;
-            this.lblGebruikersnaam.Location = new System.Drawing.Point(12, 9);
+            this.lblGebruikersnaam.Location = new System.Drawing.Point(16, 11);
+            this.lblGebruikersnaam.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGebruikersnaam.Name = "lblGebruikersnaam";
-            this.lblGebruikersnaam.Size = new System.Drawing.Size(87, 13);
+            this.lblGebruikersnaam.Size = new System.Drawing.Size(117, 17);
             this.lblGebruikersnaam.TabIndex = 3;
             this.lblGebruikersnaam.Text = "Gebruikersnaam:";
             // 
             // lblWachtwoord
             // 
             this.lblWachtwoord.AutoSize = true;
-            this.lblWachtwoord.Location = new System.Drawing.Point(12, 60);
+            this.lblWachtwoord.Location = new System.Drawing.Point(16, 74);
+            this.lblWachtwoord.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWachtwoord.Name = "lblWachtwoord";
-            this.lblWachtwoord.Size = new System.Drawing.Size(71, 13);
+            this.lblWachtwoord.Size = new System.Drawing.Size(90, 17);
             this.lblWachtwoord.TabIndex = 4;
             this.lblWachtwoord.Text = "Wachtwoord:";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(156, 174);
+            this.ClientSize = new System.Drawing.Size(213, 214);
             this.Controls.Add(this.lblWachtwoord);
             this.Controls.Add(this.lblGebruikersnaam);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtWachtwoord);
             this.Controls.Add(this.txtGebruikersnaam);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.Text = "login";
             this.ResumeLayout(false);
